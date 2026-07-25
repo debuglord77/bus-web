897863e10643597e473680047cec8d8fbcdf15c2
debuglord77 <lathishkumar858@gmail.com>
2026-07-25 16:49:51 +0530

    Update Firsthalf.jsx

--- src/components/Firsthalf.jsx
+++ src/components/Firsthalf.jsx
@@ -1,9 +1,6 @@
 function Firsthalf(){
     return (
-        <><div className=" w-full h-full bg-blue-300  ">
-            
-        
-        
+        <><div className=" w-full h-full bg-blue-300  ">   
         </div>
         
         </>
