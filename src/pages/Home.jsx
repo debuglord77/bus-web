e3d829f517e2cfab6492eb0f06df817efbaae40d
debuglord77 <lathishkumar858@gmail.com>
2026-07-25 09:11:31 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Bottomhalf from "../components/Bottomhalf";
 
 function Home() {
   return (
-    <div className="w-full h-full flex justify-center items-center bg-gray-200">
+    <div className="relative w-full h-full flex justify-center items-center bg-gray-200">
       
         <Firsthalf />
         <Bottomhalf />
