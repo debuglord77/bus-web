1a35f57130565316c94a04972d2ddbefda319543
debuglord77 <lathishkumar858@gmail.com>
2026-07-25 09:44:33 +0530

    Remove black rounded div from Firsthalf component
    Removed a black rounded div from the Firsthalf component.

--- src/components/Firsthalf.jsx
+++ src/components/Firsthalf.jsx
@@ -2,7 +2,7 @@ function Firsthalf(){
     return (
         <><div className="relative w-full h-90 bg-blue-300  ">
             
-        <div className="absolute bottom-2 left-1/2 -translate-x-1/2 w-20 h-2 bg-black rounded-full"></div>
+        
         
         </div>
         
