b981d21f3eb78ee99d1524a708b696513567dd5e
debuglord77 <lathishkumar858@gmail.com>
2026-07-25 09:20:53 +0530

    Update Firsthalf.jsx

--- src/components/Firsthalf.jsx
+++ src/components/Firsthalf.jsx
@@ -1,6 +1,6 @@
 function Firsthalf(){
     return (
-        <><div className="relative w-full h-full bg-blue-300  ">
+        <><div className="relative w-full h-90 bg-blue-300  ">
             
         <div className="absolute bottom-2 left-1/2 -translate-x-1/2 w-20 h-2 bg-black rounded-full"></div>
         
