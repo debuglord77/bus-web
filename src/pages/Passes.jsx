23c92056a704709f5af67a2fa59a27d4c859b16f
debuglord77 <lathishkumar858@gmail.com>
2026-07-26 22:17:35 +0530

    Updated UI

--- src/pages/Passes.jsx
+++ src/pages/Passes.jsx
@@ -53,7 +53,7 @@ const seconds = String(timeLeft % 60).padStart(2, "0");
                 <img
                     src={p_image}
                     alt=""
-                    className="absolute inset-0 w-full h-full object-cover rounded-4xl"
+                    className=" w-full h-full object-cover rounded-4xl"
                 />
 
 
