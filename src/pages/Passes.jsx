89774ff9895aac301a3082745825d6bcda35867c
debuglord77 <lathishkumar858@gmail.com>
2026-07-26 22:11:34 +0530

    Updated UI

--- src/pages/Passes.jsx
+++ src/pages/Passes.jsx
@@ -31,7 +31,7 @@ const hours = String(Math.floor(timeLeft / 3600)).padStart(2, "0");
 const minutes = String(Math.floor((timeLeft % 3600) / 60)).padStart(2, "0");
 const seconds = String(timeLeft % 60).padStart(2, "0");
   return (
-    <div className="relative w-full h-screen bg-black overflow-hidden flex flex-col">
+    <div className="relative w-full border-2 border-green-500 h-screen bg-black overflow-hidden flex flex-col">
       
 
         <div className="w-full h-17 border-2 flex flex-row pt-4">
@@ -80,7 +80,7 @@ const seconds = String(timeLeft % 60).padStart(2, "0");
 
 
         {/* bottom nav */}   
-        <div className=" fixed bottom-0 left-0 w-full h-20 bg-black z-50">
+        <div className=" absolute bottom-0 left-0 w-full h-20 bg-black ">
             <div className="w-full h-20 pt-1.5 px-4 border-none border-black  flex flex-row  justify-between">
 
                 <button
